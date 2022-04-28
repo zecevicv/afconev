28cbcdfdb56f92c04eda00689cfeddfa78ae4b4f
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2022-04-28 12:10:09 +0200

    [ADD] - Homepage (fully)

--- js/app.js
+++ js/app.js
@@ -16,3 +16,53 @@ if (document.querySelector('.post-slider .swiper')) {
     }
   });
 }
+
+/* #Customer Testimonials Animation
+  ======================================================= */
+if (document.querySelector('.customer-testimonials')) {
+  gsap.from(".customer-testimonials .card", {
+    yPercent: 40,
+    opacity: 0,
+    stagger: 0.05,
+    duration: .8,
+    ease: "back.out(2)",
+    scrollTrigger: {
+      trigger: ".customer-testimonials .heading-wrap",
+    },
+  });
+}
+
+/* #Station Locations Animation
+  ======================================================= */
+if (document.querySelector('.station-locations') && window.innerWidth > 1024) {
+  gsap.from(".station-locations .img", {
+    yPercent: 60,
+    scrollTrigger: {
+      trigger: ".station-locations",
+      scrub: true,
+    },
+  });
+}
+
+/* #Produt Slider
+  ======================================================= */
+if (document.querySelector('.product-slider')) {
+  swiper = new Swiper(".product-slider .swiper", {
+    pagination: {
+      el: ".product-slider .swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".product-slider .next",
+      prevEl: ".product-slider .prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      1024: {
+        slidesPerView: 1.35
+      }
+    }
+  });
+}
